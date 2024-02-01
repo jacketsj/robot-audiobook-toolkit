32076a88083229cbef3d350751d70e6e6bc2be2a
jacketsj <jacketsj@gmail.com>
2024-02-01 01:36:05 -0800

    Add ability to specify source voice

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 ENV_NAME="venv"
 
-# Check if the virtual environment exists, create it if it doesn't
 if [ ! -d "$ENV_NAME" ]; then
     echo "Virtual environment not found. Creating one..."
     python3 -m venv $ENV_NAME
@@ -11,22 +10,22 @@ else
     echo "Virtual environment already exists."
 fi
 
-# Activate the virtual environment
 source $ENV_NAME/bin/activate
 
-pip install torch torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip install -Uqq WhisperSpeech
 
-# Check if an input file is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <input_file>"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <input_file> [wav_file]"
     deactivate
     exit 1
 fi
 
-# Run the Python script with the input file as an argument
-python script.py "$1"
+if [ "$#" -eq 2 ]; then
+    python script.py "$1" --wav_file "$2"
+else
+    python script.py "$1"
+fi
 
-# Deactivate the virtual environment
 deactivate
 
--- script.py
+++ script.py
@@ -8,23 +8,28 @@ if not torch.cuda.is_available():
 
 pipe = Pipeline()
 
-def process_input_file(input_file):
+def process_input_file(input_file, wav_file=None):
+    speaker_emb = None
+    if wav_file:
+        speaker_emb = pipe.extract_spk_emb(wav_file)
+
     os.makedirs("output", exist_ok=True)
 
     with open(input_file, 'r') as file:
         for i, line in enumerate(file):
             # Skip empty lines
             if line.strip():
-                # Generate separate output for each line
                 output_filename = f"output/output_{i+1}.wav"
-                pipe.generate_to_file(output_filename, line.strip())
+                pipe.generate_to_file(output_filename, line.strip(), speaker=speaker_emb)
 
 def main():
     parser = argparse.ArgumentParser(description='Process a text file for WhisperSpeech generation.')
     parser.add_argument('input_file', type=str, help='The input text file')
+    parser.add_argument('--wav_file', type=str, help='Optional WAV file for speaker embedding', default=None)
     args = parser.parse_args()
 
-    process_input_file(args.input_file)
+    process_input_file(args.input_file, args.wav_file)
 
 if __name__ == "__main__":
     main()
+
